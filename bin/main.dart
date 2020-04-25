c55207d19379089634bba39d7de77ca981212a5c
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-25 19:16:42 -0300

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,31 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  Valores.vezesCLicado += 1;
+}
+
+class Pessoa {
+
+}
+
+class Valores {
+  /*
+   * Varáveis static são variáveis da classe,
+   * não precisando instanciar objetos para
+   * acessar essa variável.
+  */
+  static int vezesCLicado;
+
+  /*
+   * Variáveis const são constantes da classe,
+   * assim não podendo serem alteradas
+  */
+  static const int numero = 3;
+
+  /*
+   * O final é utilizado para variáveis objetos,
+   * tornando a variável imutável, ou seja,
+   * pessoa não pode receber um outro objeto
+  */
+  final pessoa = new Pessoa();
+
+  // pessoa = new Pessoa(); // == ERRO
 }
